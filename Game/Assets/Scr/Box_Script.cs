89067c814ba28c47f95bc58559bf78297c45b472
pakotaba <tabatadzepavle@gmail.com>
2016-05-14 12:48:41 +0400

    პროექტის დაბადება
    ყუთის მოძრაობა, ბურთის ვარდნა

---
+++ Game/Assets/Scr/Box_Script.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box_Script : MonoBehaviour {
+
+	void Start ()
+    {
+        
+            
+	}
+	
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) && gameObject.transform.position.x >= -2.3f)
+        {
+            gameObject.transform.Translate(-0.1f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && gameObject.transform.position.x <=2.3f)
+        {
+            gameObject.transform.Translate(0.1f, 0, 0);
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        Destroy(other.gameObject);
+    } 
+}
